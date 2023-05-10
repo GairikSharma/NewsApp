81b84c83c8bdbc5090af68d28dd8908a81e5bf25
Gairik Sharma <gairiksharma202@gmail.com>
2023-05-10 13:50:17 +0530

    Update

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,8 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import Newsapi from "./pages/newsapi";
-import Article from "./pages/sports";
+import Sports from "./pages/sports";
+import Business from "./pages/business";
 import NewsapiBusiness from "./pages/business";
 import NewsapiArticles from "./pages/sports";
 import Navbar from "./components/Navbar/navbar";
@@ -15,8 +16,8 @@ function Routing() {
     <Navbar />
       <Routes>
         <Route path="/" element={<Newsapi />}></Route>
-        <Route path="/business" element=<h1>Business</h1>></Route>
-        <Route path="/sports" element=<h1>Sports</h1>></Route>
+        <Route path="/business" element={<Business />}></Route>
+        <Route path="/sports" element={<Sports />}></Route>
       </Routes>
     </BrowserRouter>
   );
